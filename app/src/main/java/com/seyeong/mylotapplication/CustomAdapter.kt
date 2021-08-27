200f0df8dd3301abc0ea533edc083fbf24c124a8
ys200209 <lys200209@naver.com>
2021-08-27 23:00:29 +0900

    Example CustomAdapter & HttpURLConnection

--- app/src/main/java/com/seyeong/mylotapplication/CustomAdapter.kt
+++ app/src/main/java/com/seyeong/mylotapplication/CustomAdapter.kt
@@ -1,15 +1,30 @@
 package com.seyeong.mylotapplication
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.pm.PackageInfoCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.seyeong.mylotapplication.databinding.ItemRecyclerBinding
 
-class CustomAdapter: RecyclerView.Adapter<Holder>()  {
+class CustomAdapter(val context: Context): RecyclerView.Adapter<CustomAdapter.Holder>()  {
+    val imageList = MainActivity().imageList
     var lot_list = mutableListOf<Lotto>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        /*for( i in 1..45) {
+            var bmp : Int = context.getResources().getIdentifier("lot_"+i, "drawable", "com.seyeong.mylotapplication")
+            var bitmap:Bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(), bmp), 38,45, false)
+            imageList.add(bitmap)
+        }*/
+
+        Log.d("태그", "(onCreateViewHolder) 종료.")
 
         return Holder(binding)
     }
@@ -19,18 +34,29 @@ class CustomAdapter: RecyclerView.Adapter<Holder>()  {
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
+
         val lotto = lot_list.get(position) // 아직 아무것도 안넣었는데 뭔 .get ?
+
+        holder
+
+        Log.d("태그", "(onBindViewHolder) setLotto 메서드 호출.")
         holder.setLotto(lotto)  //
     }
 
+    inner class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
 
-
-}
-
-class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
-
-    fun setLotto(lotto: Lotto) {
+        fun setLotto(lotto: Lotto) {
+            binding.number1.setImageBitmap(imageList.get(0))
+            binding.number2.setImageBitmap(imageList.get(1))
+            binding.number3.setImageBitmap(imageList.get(2))
+            binding.number4.setImageBitmap(imageList.get(3))
+            binding.number5.setImageBitmap(imageList.get(4))
+            binding.number6.setImageBitmap(imageList.get(5))
+            binding.bonusNumber.setImageBitmap(imageList.get(6))
+            Log.d("태그", "Holder.setLotto 메서드 종료")
+        }
 
     }
 
 }
+
--- app/src/main/java/com/seyeong/mylotapplication/MainActivity.kt
+++ app/src/main/java/com/seyeong/mylotapplication/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.seyeong.mylotapplication
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.seyeong.mylotapplication.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -21,71 +22,117 @@ class MainActivity : AppCompatActivity() {
     var jsonArray = JSONArray()
     var jsonObject = JSONObject()
     var drwNo: Int = 0
-
+    val imageList = mutableListOf<Bitmap>()
+    var lottoData: Deferred<MutableList<Lotto>>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        for( i in 1..45) {
+            var bmp : Int = this.resources.getIdentifier("lot_"+i, "drawable", "com.seyeong.mylotapplication")
+            var bitmap:Bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.resources, bmp), 38,45, false)
+            imageList.add(bitmap)
+        }
+
+        Log.d("태그", "시작...")
+        var adapter = CustomAdapter(this)
+
+
+        CoroutineScope(Dispatchers.Default).async {
+            lottoData = async {
+                loadLotto()
+            }
+            if (lottoData.await() != null )
+            adapter.lot_list = lottoData.await()
+
+            Log.d("태그", "adapter 생성.")
+            binding.recyclerView.adapter = adapter
+            Log.d("태그", "어댑터 연결.")
+            binding.recyclerView.layoutManager = LinearLayoutManager(binding.root.context)
+            Log.d("태그", "LayoutManager 설정.")
+        }
         /*binding.button.setOnClickListener {
-            try {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val url = URL(address)
-                    val urlConnection = url.openConnection() as HttpURLConnection
+
+
+        }*/
+    }
+
+    fun loadLotto(): MutableList<Lotto> {
+        val data: MutableList<Lotto> = mutableListOf()
+
+        try {
+            CoroutineScope(Dispatchers.Default).async {
+                val url = URL(address)
+                val urlConnection = url.openConnection() as HttpURLConnection
+                val asyncStr = async {
                     if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
                         val streamReader = InputStreamReader(urlConnection.inputStream)
                         val buffered = BufferedReader(streamReader)
 
                         while (true) {
-                            var line = buffered.readLine()?: break
+                            var line = buffered.readLine() ?: break
                             str.append(line)
                         }
                         buffered.close()
                         urlConnection.disconnect()
-                    }
-                    Log.d("태그", "str = ${str}")
 
+                        str
+                    } else {
+                        null
+                    }
+                }
+                Log.d("태그", "asyncStr = ${asyncStr.await()}")
+
+                /*launch(Dispatchers.Main) {
+                    /*
+                    binding.textView.text = str.toString()
+                    binding.textView2.text = "${jsonObject.getInt("drwNo")}회\n" +
+                            "${jsonObject.getInt("drwtNo1")}, ${jsonObject.getInt("drwtNo2")}, " +
+                            "${jsonObject.getInt("drwtNo3")}, ${jsonObject.getInt("drwtNo4")}, " +
+                            "${jsonObject.getInt("drwtNo5")}, ${jsonObject.getInt("drwtNo6")} \n" +
+                            "${jsonObject.getInt("firstWinamnt")}원"*/
+
+                }*/
+
+                if ( asyncStr.await() != null) {
+                    Log.d("태그", "data.add( Lotto(...) )")
 
                     jsonArray = JSONArray("[${str}]")
                     jsonObject = jsonArray.getJSONObject(0)
 
-                    launch(Dispatchers.Main) {
-                        binding.textView.text = str.toString()
-                        binding.textView2.text = "${jsonObject.getInt("drwNo")}회\n" +
-                                "${jsonObject.getInt("drwtNo1")}, ${jsonObject.getInt("drwtNo2")}, " +
-                                "${jsonObject.getInt("drwtNo3")}, ${jsonObject.getInt("drwtNo4")}, " +
-                                "${jsonObject.getInt("drwtNo5")}, ${jsonObject.getInt("drwtNo6")} \n" +
-                                "${jsonObject.getInt("firstWinamnt")}원"
-                    }
+                    data.add(
+                        Lotto(
+                            jsonObject.getString("returnValue"),
+                            jsonObject.getString("drwNoDate"),
+                            jsonObject.getInt("firstPrzwnerCo"),
+                            jsonObject.getLong("firstWinamnt"),
+                            jsonObject.getInt("drwNo"),
+                            jsonObject.getInt("drwtNo1"),
+                            jsonObject.getInt("drwtNo2"),
+                            jsonObject.getInt("drwtNo3"),
+                            jsonObject.getInt("drwtNo4"),
+                            jsonObject.getInt("drwtNo5"),
+                            jsonObject.getInt("drwtNo6"),
+                            jsonObject.getInt("bnusNo")
+                        )
+                    )
 
+                    Log.d("태그", "data = ${data.toString()}")
+                    Log.d("태그", "data = ${data.size}")
                 }
 
-
-            } catch (e: Exception) {
-                Log.d("태그", "Error : $e")
-                e.printStackTrace()
             }
+        } catch (e: Exception) {
+            Log.d("태그", "Error : $e")
+            e.printStackTrace()
+        }
 
-        }*/
+        return data
+    }
+
+    suspend fun jsonConnect() {
+
     }
 
 }
-/*
-suspend fun loadUrl(url: String) : String {
-    val str1 = URL(url)
-    val urlConnection = str1.openConnection() as HttpURLConnection
-    val content = StringBuilder()
-    urlConnection.requestMethod = "GET"
-    if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
-        val streamReader = InputStreamReader(urlConnection.inputStream)
-        val buffered = BufferedReader(streamReader)
 
-        while (true) {
-            val line = buffered.readLine()?: break
-            content.append(line)
-        }
-        Log.d("태그", "content = ${content}")
-        buffered.close()
-        urlConnection.disconnect()
-    }
-    return content.toString()
-}*/
