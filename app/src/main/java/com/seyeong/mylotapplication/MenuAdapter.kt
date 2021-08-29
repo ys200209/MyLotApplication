d74ff4d3f339e174e24b2dd57e4b61bebed1c3e8
ys200209 <lys200209@naver.com>
2021-08-29 21:29:53 +0900

    Create Menu Adapter

---
+++ app/src/main/java/com/seyeong/mylotapplication/MenuAdapter.kt
@@ -0,0 +1,30 @@
+package com.seyeong.mylotapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.seyeong.mylotapplication.databinding.ItemRecyclerBinding
+
+class MenuAdapter: RecyclerView.Adapter<Holder>() {
+    var menuList = mutableListOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return Holder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return menuList.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val menu = menuList.get(position)
+        holder.setMenu(menu)
+    }
+
+}
+
+class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+
+}
